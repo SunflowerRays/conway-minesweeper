5764030562d6840cb4288524fe5fb44b3fbb8f23
Vivien White <mwhitecwc@hotmail.co.uk>
2026-04-25 18:32:06 +0100

    Pattern setter now works with mouse inputs, and the generator can be activated by pressing the spacebar,

--- Assets/Scripts/GoL.cs
+++ Assets/Scripts/GoL.cs
@@ -4,20 +4,20 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
+using static MouseHandler;
+using static UnityEngine.InputSystem.HID.HID;
 
 public class GoL : MonoBehaviour
 {
-    [SerializeField] private Tilemap currentState;
+    [SerializeField] public Tilemap currentState;
     [SerializeField] private Tilemap nextState;
-    [SerializeField] private Tile aliveTile;
+    [SerializeField] public Tile aliveTile;
     [SerializeField] private Tile deadTile;
-    //[SerializeField] private Tile borderTile;
-    //[SerializeField] private Pattern pattern;
-    public HashSet<(int x, int y)> pattern { get; set; }
 
     [SerializeField] public float freqInterval = 0.20f;
     [SerializeField] private int gridWidth = 12;
     [SerializeField] private int gridHeight = 12;
+
     public (int x, int y) centre;
     public Grid grid;
     public LiveRegistry liveRegistry;
@@ -26,15 +26,17 @@ public class GoL : MonoBehaviour
     public MineHider mineHider;
     public PatternManager patternManager;
     [SerializeField] public HashSet2TileMap HashSet2TileMap;
+    [SerializeField] public MouseHandler mouseHandler;
+
 
     public int iterations { get; internal set; }
     public float time { get; internal set; }
 
     private void Awake()
     {
-        pattern = new HashSet<(int x, int y)>();
+        centre = (0, 0);
         liveRegistry = new LiveRegistry();
-        patternManager = new PatternManager();
+        patternManager = new PatternManager(liveRegistry);
         grid = new Grid(centre, gridWidth, gridHeight);
         mineDetector = new MineDetector(grid, liveRegistry);
         mineHider = new MineHider(grid);
@@ -44,38 +46,11 @@ public class GoL : MonoBehaviour
     public void Start()
     {
 
-        SetPattern(pattern);
-        if (generator != null)
-        {
-            StartCoroutine(generator.Simulate());
-        }
-    }
-
-    private void SetPattern(HashSet<(int x, int y)> pattern)
-    {
-        Clear();
-
-        (int x, int y) localCentre = (0, 0);
-        localCentre = patternManager.GetCentre(pattern);
-
-
-        foreach (var (x, y) in pattern)
-        {
-            //Vector3Int cell = new Vector3Int(x - localCentre.x, y - localCentre.y, 0);
-
-
-            //currentState.SetTile(cell, aliveTile);
-            // liveRegistry.aliveCells.Add(cell);
-            liveRegistry.newAliveCells.Add((x, y));
-        }
-
-
-
-        //liveRegistry.population = liveRegistry.aliveCells.Count;
+        patternManager.SetPattern();
         liveRegistry.population = liveRegistry.newAliveCells.Count;
-        centre = localCentre;
-        mineHider.coverMines(grid);
+        //mineHider.coverMines(grid);
         HashSet2TileMap.mapper(liveRegistry.newAliveCells, currentState);
+
     }
 
     private void Clear()
@@ -96,7 +71,17 @@ public class GoL : MonoBehaviour
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            generator.Stop();
+            if (!generator.isRunning)
+            {
+                patternManager.SetPattern();
+                mouseHandler.SetMode(MouseHandler.GameMode.Simulating);
+                StartCoroutine(generator.Simulate());
+            }
+            else
+            {
+                generator.Stop();
+                mouseHandler.SetMode(MouseHandler.GameMode.Minesweeper);
+            }
         }
     }
 
@@ -104,12 +89,6 @@ public class GoL : MonoBehaviour
     private void OnEnable()
     {
 
-        /* reactivate if restarting the generator becomes necessary later.
-        if (generator != null)
-        {
-            StartCoroutine(generator.Simulate());
-        }
-        */
     }
 
     private void OnDisable()
--- Assets/Scripts/HashSet2TileMap.cs
+++ Assets/Scripts/HashSet2TileMap.cs
@@ -22,6 +22,9 @@ public class HashSet2TileMap : MonoBehaviour
     [SerializeField] private Tilemap greyfield;
     [SerializeField] private Tilemap currentState;
     [SerializeField] private Tilemap nextState;
+
+
+
     /// <summary>
     /// Read HashSet write to tilemap.
     /// </summary>
@@ -44,7 +47,6 @@ public class HashSet2TileMap : MonoBehaviour
                 tileMap.SetTile(new Vector3Int(cell.x, cell.y, 0), greyTile);
             }
         }
-
     }
 
     public void mapper(List<MineDetector.CellData> cellsData, Tilemap tileMap)
@@ -76,7 +78,28 @@ public class HashSet2TileMap : MonoBehaviour
     }
 
 
-    //TODO: Read tilemap method.
+    public HashSet<(int x, int y)> readTileMap(Tilemap tileMap)
+    {
+        HashSet<(int x, int y)> temp = new HashSet<(int x, int y)>();
+
+        foreach (var position in tileMap.cellBounds.allPositionsWithin)
+        {
+            if (tileMap.GetTile(position) != null)
+            {
+                temp.Add((position.x, position.y));
+            }
+        }
+
+        return temp;
+    }
+
+    public (int width, int height) getTilemapDimensions(Tilemap tileMap)
+    {
+        // add comments
+        BoundsInt bounds = tileMap.cellBounds;
+        return (bounds.size.x, bounds.size.y);
+    }
+
 
 
     void Awake()
