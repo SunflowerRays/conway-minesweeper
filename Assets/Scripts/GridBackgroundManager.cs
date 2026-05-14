f9ab46a7518da196f31bb801b11cb82a88320d16
Vivien White <mwhitecwc@hotmail.co.uk>
2026-05-14 18:20:54 +0100

    grid centred.

--- Assets/Scripts/GridBackgroundManager.cs
+++ Assets/Scripts/GridBackgroundManager.cs
@@ -4,12 +4,30 @@ public class GridBackgroundManager : MonoBehaviour
 {
     [SerializeField] private GoL gol;
     [SerializeField] private SpriteRenderer background;
+    
+
+    [SerializeField] private float verticalOffset = 0f;
 
     void Start()
     {
-        float width = gol.grid.gridWidth;
-        float height = gol.grid.gridHeight;
-        background.transform.localPosition = new Vector3(0, 0, 0);
-        background.transform.localScale = new Vector3(width, height, 1);
+        float centreX = gol.grid.centre.x + (gol.grid.gridWidth / 2f) - 0.5f;
+        float centreY = gol.grid.centre.y + (gol.grid.gridHeight / 2f) - 0.5f;
+
+        Camera.main.transform.position = new Vector3(-0.5f, -0.5f + verticalOffset, Camera.main.transform.position.z);
+        Camera.main.orthographicSize = gol.grid.gridHeight / 2f + 1f;
+
+
     }
+
+    // Old start
+    //void Start()
+    //{
+
+
+
+    //    float width = gol.grid.gridWidth;
+    //    float height = gol.grid.gridHeight;
+    //    background.transform.localPosition = new Vector3(0, 0, 0);
+    //    background.transform.localScale = new Vector3(width, height, 1);
+    //}
 }
